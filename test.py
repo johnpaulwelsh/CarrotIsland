64bd1de5ba53f971d788bfe437e46db051aa0bfd
Nicholas La Roux <larouxn@gmail.com>
2012-11-02 02:46:06 -0400

    Complete Rewrite
    Found great file online, copy pasta until we understand this stuff.

--- test.py
+++ test.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python
-from Tkinter
+from Tkinter import *
 
-# Create root window with title
-root = Tkinter.Tk()
-root.title("Thomas Edison: Paranormal Inquisitor")
-           
-# Define the canvas
-width = 960;
-height = 540;
+def create_main():
+    # Creates title
+    frame1 = Frame(top, relief=GROOVE, borderwidth=2)
+    label1 = Label(frame1, text='Thomas Edison: Paranormal Inquisitor', font=('Arial', 48), fg="blue")
+    label1.pack(side="left")
+    
+    # Creates window
+    top.create_window(540, 300, window=frame1, anchor=SE)
+    top.pack(fill="full", expand="yes")
+    
+    # Creates start button
+    frame2 = Frame(top, relief=GROOVE, borderwidth=2)
+    button1 = Button(frame2, text='Start', command=go_game, font=('Arial', 28), fg="blue")
+    button1.pack(side="left")
+    
+    top.create_window(457, 450, window=frame2, anchor=SE)
+    top.pack(fill='both', expand='yes')
+    
+    # Creates option button
+    frame3 = Frame(top, relief=GROOVE, borderwidth=2)
+    button2 = Button(frame3, text='Options', command=go_options, font=('Arial', 28), fg="blue")
+    button2.pack(side="left")
+    
+    top.create_window(480, 550, window=frame3, anchor=SE)
+    top.pack(fill='both', expand='yes')
+    
+    #Creates exit button
+    frame5 = Frame(top, relief=GROOVE, borderwidth=2)
+    button4 = Button(frame5, text='Quit',command=quit, font=('Arial', 28), fg="blue")
+    button4.pack(side="left")
+    
+    top.create_window(455, 750, window=frame5, anchor=SE)
+    top.pack(fill='both', expand='yes')
+    
+    return top
 
-# Invoke canvas
-c = Canvas(width=width, height=height, bg='black')
-c.pack()
-
-main.mainloop()
+# Still needs a lot more
