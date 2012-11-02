d01f8f02ef6ac1ec5ea8a7111c13b778797f65be
Nicholas La Roux <larouxn@gmail.com>
2012-11-02 02:15:54 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from Tkinter
+
+# Create root window with title
+root = Tkinter.Tk()
+root.title("Thomas Edison: Paranormal Inquisitor")
+           
+# Define the canvas
+width = 960;
+height = 540;
+
+# Invoke canvas
+c = Canvas(width=width, height=height, bg='black')
+c.pack()
+
+main.mainloop()
