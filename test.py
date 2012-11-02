07aa1df4a7aa3d4144d61921c19e5f2f5f80bef9
Nicholas La Roux <larouxn@gmail.com>
2012-11-02 13:48:21 -0400

    Buttons, Buttons Everywhere
    Added Buttons. Second Buttons commit, maybe soon with <hr> across
    bottom.

--- test.py
+++ test.py
@@ -4,31 +4,48 @@
 """
 ZetCode Tkinter tutorial
 
-This script shows a simple window
-on the screen.
+This program creates a quit
+button. When we press the button,
+the application terminates. 
 
 author: Jan Bodnar
-last modified: January 2011
+last modified: December 2010
 website: www.zetcode.com
 """
 
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import Tk, Label, BOTH
+from ttk import Frame, Button, Style
 
 
 class Example(Frame):
   
     def __init__(self, parent):
-        Frame.__init__(self, parent, background="black")   
+        Frame.__init__(self, parent)   
          
         self.parent = parent
         
         self.initUI()
-    
+        
     def initUI(self):
       
         self.parent.title("Thomas Edison : Paranormal Inquisitor")
+        self.style = Style()
+        self.style.theme_use("clam")
+
         self.pack(fill=BOTH, expand=1)
         
+        quitButton = Button(self, text="Play",
+            command=self.quit)
+        quitButton.place(x=125, y=450)
+        
+        quitButton = Button(self, text="Options",
+            command=self.quit)
+        quitButton.place(x=450, y=450)
+        
+        quitButton = Button(self, text="Quit",
+            command=self.quit)
+        quitButton.place(x=775, y=450)
+
 
 def main():
   
