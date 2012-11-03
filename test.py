9890d59739486e6108983a14b6b09e460fd30b79
Michael <michael@ubuntu.(none)>
2012-11-03 05:44:33 -0400

    Added a feature to the options button that pops up with back button that upon press returns the Options button, will later be replaced when I can figure out how to make it so that we can use it to go to a whole list of options and then back to ALL of the original buttons.

--- test.py
+++ test.py
@@ -13,11 +13,10 @@ last modified: December 2010
 website: www.zetcode.com
 """
 
-from Tkinter import Tk, Label, BOTH
-from ttk import Frame, Button, Style
+from Tkinter import *
+from ttk import *
 
-
-class Example(Frame):
+class Window(Frame):
   
     def __init__(self, parent):
         Frame.__init__(self, parent)   
@@ -33,25 +32,53 @@ class Example(Frame):
         self.style.theme_use("clam")
 
         self.pack(fill=BOTH, expand=1)
-        
-        quitButton = Button(self, text="Play",
+        """
+        set
+        x = 427.5 
+        and 
+        y = 270 + or - 45 
+        (30 for size of button,
+         15 for spacing)
+        """
+        playButton = Button(self, text="Play",
             command=self.quit)
-        quitButton.place(x=125, y=450)
+        playButton.place(x=427.5, y=225)
         
-        quitButton = Button(self, text="Options",
-            command=self.quit)
-        quitButton.place(x=450, y=450)
+        optionButton = Button(self, text="Options",
+            command=self.optionUI)
+        optionButton.place(x=427.5, y=270)
         
         quitButton = Button(self, text="Quit",
             command=self.quit)
-        quitButton.place(x=775, y=450)
-
+        quitButton.place(x=427.5, y=315)
+        """
+        Option UI is going 
+        to replace the
+        main screen when
+        anyone clicks on 
+        the Option Button. 
+        """
+    def optionUI(self):
+        playButton = Button(self, text="Play",
+                            command=self.quit)
+        playButton.size = 0
+        
+        self.parent.title("Thomas Edison : Paranormal Inquisitor")
+        self.style = Style()
+        self.style.theme_use("clam")
+        
+        self.pack(fill=BOTH, expand = 1)
+        
+        configButton = Button(self, text = "Back",
+                              command=self.initUI)
+        configButton.place(x=427.5, y=270)
+        
 
 def main():
   
     root = Tk()
     root.geometry("960x540+300+300")
-    app = Example(root)
+    app = Window(root)
     root.mainloop()  
 
 
